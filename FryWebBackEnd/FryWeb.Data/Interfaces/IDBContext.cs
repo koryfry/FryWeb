9c629fe40d37daa9fc2aaba8b895a4e34a785189
koryfry <smallfry89@sbcglobal.net>
2019-05-11 12:32:00 -0400

    Missed some files

---
+++ FryWebBackEnd/FryWeb.Data/Interfaces/IDBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+
+namespace FryWeb.Data.Interfaces
+{
+    public interface IDBContext : IDisposable
+    {
+        IDbConnection Connection { get; }
+
+        T QueryFirst<T>(string query, object param = null, CommandType commandType = CommandType.Text, IDbTransaction transaction = null);
+        IEnumerable<T> Query<T>(string query, object param = null, CommandType commandType = CommandType.Text, IDbTransaction transaction = null);
+        SqlMapper.GridReader QueryMultiple(string query, object param = null, Action<SqlMapper.GridReader> map = null, CommandType commandType = CommandType.Text, IDbTransaction transaction = null);
+        IEnumerable<T1> Query1To1<T1, T2>(string childProperty, string splitOn, string query, object param = null, CommandType commandType = CommandType.Text, IDbTransaction transaction = null);
+        IEnumerable<T1> Query1ToN<T1, T2>(string parentIDProperty, string childProperty, string splitOn, string query, object param = null, CommandType commandType = CommandType.Text, IDbTransaction transaction = null);
+        int Command(string sql, object param = null, CommandType commandType = CommandType.Text, IDbTransaction transaction = null);
+        T Transaction<T>(Func<IDbTransaction, T> query);
+        IDbTransaction BeginTransaction();
+        void Commit();
+        void Rollback();
+    }
+}
---
+++ FryWebBackEnd/FryWeb.Data/Interfaces/IQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+
+namespace FryWeb.Data.Interfaces
+{
+    public interface IQuery<T>
+    {
+        string Sql { get; set; }
+
+        T Execute(IDBContext context, IDbTransaction transaction = null);
+    }
+}
